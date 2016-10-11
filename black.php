08beb1bac807fa28e47de3c51437a4a15052e8b4
starwarsgood <comp822012016@mail.ru>
2016-10-11 17:47:29 +0300

    gggg

---
+++ black.php
@@ -0,0 +1,20 @@
+﻿<?php
+ class black{
+	 private $lo,$ol,$lol;
+     function __construct($lo,$ol){
+		 $this -> lo = $lo;
+		 $this -> ol = $ol;
+	 }
+	 function sum(){
+		 $this -> lol = $this -> lo + $this -> ol;
+	 }
+	function sume(){
+		return $this -> lol;
+	}
+	
+   function __destruct() {
+       echo "Уничтожается "  "\n";
+   }
+ };
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+﻿<?php
+require_once 'black.php';
+$fuf= new black(2,3);
+$fuf -> sum();
+$a = $fuf ->sume();
+echo $a;
+?>
